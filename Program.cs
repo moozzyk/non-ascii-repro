0a39f73c49e7dba465aab10c6df4e630df71dc2c
moozzyk <pawelka@microsoft.com>
2016-06-10 13:06:02 -0700

    Changing repro to avoid file system

--- Program.cs
+++ Program.cs
@@ -9,10 +9,13 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             const string text = "Hëllö Wörld!";
-            var encoding = Encoding.GetEncoding("iso-8859-1");
-            File.WriteAllText("out.txt", text, encoding);
-            var contents = File.ReadAllText("out.txt", encoding);
-            Console.WriteLine(contents == text);
+
+            foreach (var c in text)
+            {
+                Console.Write("{0:x} ", (int)c);
+            }
+
+            Console.WriteLine();
         }
     }
 }
